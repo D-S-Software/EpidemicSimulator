4c404742fa0c614421dca342ec687267f25b2d9b
Stra2020 <lukestrachan55@gmail.com>
2020-04-19 22:04:39 -0400

    Person Class update7

--- src/Engine.java
+++ src/Engine.java
@@ -1,3 +1,5 @@
+import java.util.Timer;
+
 public class Engine {
 
     private Board myBoard;
@@ -7,13 +9,12 @@ public class Engine {
         myBoard = new Board();
     }
 
-    public Board getBoard()
-    {
-        return myBoard;
-    }
-
     public static void main(String[] args)
     {
+        Engine myEngine = new Engine();
+        Frame screen = new Frame(myEngine.myBoard);
+
+        Timer timer = new Timer(10, );
 
     }
 }
