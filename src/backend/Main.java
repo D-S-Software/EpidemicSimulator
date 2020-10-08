f9513f1914a023528a532311864ca21faab2d718
Luke <lukestrachan55@gmail.com>
2020-10-08 15:47:37 -0400

    small javadoc update

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -17,14 +17,4 @@ public class Main {
     {
         Main test = new Main();
     }
-
-
-    //TODO 5 min fix
-    /*
-    - Contagious Range plays when negative num entered on first run
-    - Use space bar to pause and play sim and arrows to speed up and slow down (enter to start)
-     */
-
-    //TODO Make opening screen (start with music) - V2
-    //TODO Central Market - V2
 }
